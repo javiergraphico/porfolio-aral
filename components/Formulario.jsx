2fd88faf09e6600f73a255a5627ee751e97b23b7
javiergraphico <javieralexander38@hotmail.com>
2023-11-15 16:27:11 -0400

    prueba dos de correo

--- components/Formulario.jsx
+++ components/Formulario.jsx
@@ -14,7 +14,7 @@ const Formulario = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch("/api/sendEmail", {
+      const response = await fetch('https://porfolio-aral.vercel.app/api/sendEmail', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
